73414e6620342baaacb89b0f9302b5a42a6d6eda
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-09-16 23:57:16 +0000

    :tada: El helado de mantecado es el mejo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const galletas = ["Cucurucho", "Canasta", "Sundae"]; // 3
+
+  const sabores = [
+    "Chocolate",
+    "Mantecado",
+    "Dulce de leche",
+    "Frutilla",
+    "Menta Granizado", // 9
+    "Pistacho",
+    "Limon",
+    "Sambayon",
+    "Pororo"
+  ];
+
+  const toppings = ["Chispitas", "M&Ms", "Rocher", "Salsa", "Gomitas"]; // 5
+
+  let cantidadDeHelados = 0;
+  let menu = [];
+
+  galletas.forEach(envase => {
+    sabores.forEach(sabor => {
+      toppings.forEach(topping => {
+        cantidadDeHelados++;
+        menu.push(
+          `${cantidadDeHelados} ${envase} ${sabor} ${topping} chef: Natali`
+        );
+      });
+    });
+  });
+
+  let menuHtml = menu.map(helado => {
+    return '<li class="alert alert-warning mt-2">' + helado + "</li>";
+  });
+  let iceCreamItem = document.getElementById("ice-cream-list");
+  iceCreamItem.innerHTML = menuHtml.join(" ");
 };
